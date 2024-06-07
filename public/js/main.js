3b4a3c1c20f104ed191c70fe842c2eb9f65977b0
Ehsan <azimiehsan@gmail.com>
2024-06-07 14:38:15 +0330

    Filimo_API_Slider_Ver_2.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,33 @@
 // import menu from "./menu/menu";
+// import { Autoplay } from "swiper/modules";
 import buttom_menu from "../js/menu/buttom_menu.js";
-
+import slider from "../js/slider/slider.js";
+// import Swiper from "swiper";
 // menu ()
-buttom_menu();
+
+function initslider() {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+async function allData() {
+  await buttom_menu();
+  await slider();
+  initslider();
+}
+
+allData();
---
+++ public/js/slider/slider.js
@@ -0,0 +1,16 @@
+const slider = async () => {
+  try {
+    let data = await fetch("http://localhost:3000/slider");
+    let res = await data.json();
+    let slider = res.map((elem) => {
+      console.log('elem.image');
+      return `<div class="swiper-slide"><img src=${elem.image} alt=${elem.alt} /> </div>`
+    });
+    document
+      .querySelector(".swiper-wrapper")
+      .insertAdjacentHTML("beforeend", slider.join(""));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export default slider;
