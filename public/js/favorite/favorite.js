83ee4d9c89a8af9d78e20df049aece3bf2652f60
Ehsan <azimiehsan@gmail.com>
2024-07-28 16:12:18 +0330

    Filimo_API_Slider_Fav_Online_Ver_4
    git commit -m Filimo_API_Slider_Fav_Online_Ver_4

--- public/js/favorite/favorite.js
+++ public/js/favorite/favorite.js
@@ -1,17 +1,17 @@
 const favorite = async () => {
-    try {
-      let data = await fetch("http://localhost:3000/favorite");
-      let res = await data.json();
-      let favorite = res.map((elem) => {
-        return `<div><img src=${elem.image} alt=${elem.alt} /><span><img src=${elem.image2} alt=${elem.alt2}></span>
+  try {
+    let data = await fetch("http://localhost:3000/favorite");
+    let res = await data.json();
+    let favorite = res.map((elem) => {
+      return `<div><a href=${elem.call_favorite_movie} id=${elem.a_id}>
+<img src=${elem.image} alt=${elem.alt} /><span><img src=${elem.image2} alt=${elem.alt2}></span></a>
         </div>`;
-      });
-      document
-        .querySelector(".favorite-movie")
-        .insertAdjacentHTML("beforeend", favorite.join(""));
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  export default favorite;  
-  
+    });
+    document
+      .querySelector(".favorite-movie")
+      .insertAdjacentHTML("beforeend", favorite.join(""));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export default favorite;
--- public/js/main.js
+++ public/js/main.js
@@ -25,11 +25,113 @@ function initslider() {
   });
 }
 
+function initsliderfavorite() {
+  var swiper = new Swiper(".mySwiper_2", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var appendNumber = 4;
+  var prependNumber = 1;
+  document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.prependSlide([
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      ]);
+    });
+  document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.prependSlide(
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+      );
+    });
+  document
+    .querySelector(".append-slide")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.appendSlide(
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+      );
+    });
+  document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.appendSlide([
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      ]);
+    });
+}
+
+function initslider_freemode() {
+  var swiper = new Swiper(".mySwiper_2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+function free_Content_swiper_coverflow() {
+  var swiper = new Swiper(".mySwiper_3", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
+
+function idea_swiper() {
+  var swiper = new Swiper(".mySwiper_idea", {
+    slidesPerView: 3,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
 async function allData() {
   await buttom_menu();
   await slider();
   await favorite();
   initslider();
+  //initsliderfavorite();
+  initslider_freemode();
+  free_Content_swiper_coverflow();
+  idea_swiper();
 }
 
 allData();
